7dce81d5db492300cf45edefb2e5b45fb2d1de86
Rodrigo Costa <57047212+rfcosta85@users.noreply.github.com>
2020-10-22 20:57:17 -0300

    Eleicoes 2020

---
+++ aula11/revisao.html/revisao008.js
@@ -0,0 +1,11 @@
+var velocidade = 87
+console.log(`A velocidade do seu carro é ${velocidade} Km/h`)
+
+if(velocidade > 60)
+{
+
+    console.log(`Você ultrapassou a velocidade máxima permitida que é de: 60Km, portanto, está MULTADO!`)
+
+}
+
+console.log('Dirija sempre usando o cinto de segurança')
