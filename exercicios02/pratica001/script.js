14b06704ea1c12c9d8667935c2631e5384e5c5cc
Rodrigo Costa <rfcosta85@gmail.com>
2020-11-06 11:50:29 -0300

    Resolução do exercicio 01 - Módulo E

--- exercicios02/pratica001/script.js
+++ exercicios02/pratica001/script.js
@@ -1,28 +1,63 @@
 function contar()
 {
 
-    var inicio = window.document.getElementById('inicio')
-    var fim = window.document.getElementById('fim')
-    var passo = window.document.getElementById('passo')
-    var resultado = window.document.getElementById('resultado')
-    var contador = inicio
-    var transforma = Number(contador.value)
+    let inicio = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let resultado = window.document.getElementById('resultado')
+    
 
-    if(inicio == null)
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 )
+    {
+        
+        resultado.innerHTML += `Impossível contar!`
+
+    }else
     {
 
-        resultado.innerHTML = 'Impossível contar!'
+        resultado.innerHTML = `Contando: <br>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0)
+        {
+
+            window.alert(`Passo inválido! Considerando PASSO 1`)
+            p = 1
+
+        }
+
+        if(i < f)
+        {
+
+            //Contagem crescente 
+
+            for(let c = i; c <= f; c += p)
+            {
+
+                resultado.innerHTML += `${c} \u{1F449} `
+
+            }         
+
+        }else 
+        {
+
+            //Contagem Regressiva
+
+            for(let c = i; c >= f; c -= p)
+            {
+
+                resultado.innerHTML += `${c} \u{1F449}`
+
+            }
+
+        }
+
+        resultado.innerHTML += `\u{1F3C1}`
 
     }
 
-    for(inicio; fim; passo)
-    {
-
-        transforma += passo
-
-        resultado.innerHTML = 'Contando:'
-        resultado.appendChild = `${transforma} > `
-
-    }        
+    
 
 }
