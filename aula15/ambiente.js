eb19e0c47a7870b9fe767a1d5de9f27e19393bef
Rodrigo Costa <rfcosta85@gmail.com>
2020-11-07 17:16:10 -0300

    Aula 15 - Arrays

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let alunos = ['André', 'Antônio', 'Jackson', 'Rodrigo', 'Yuri', 'Alexandre', 'Leonardo']
+alunos.sort()
+
+for(let i in alunos)
+{
+
+    console.log(`Aluno ${i}: ${alunos[i]}`)
+
+}
