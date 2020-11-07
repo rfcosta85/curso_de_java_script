d0d56cdaba7b44ff64b86cc4ffbae40025f96265
Rodrigo Costa <rfcosta85@gmail.com>
2020-11-07 18:14:53 -0300

    aula 16 - exemplos 1 e 2

---
+++ aula16/funcao.js
@@ -0,0 +1,19 @@
+function parimpar(numero)
+{
+
+    if(numero % 2 == 0)
+    {
+
+        return 'Par!'
+
+    }else 
+    {
+
+        return 'Ímpar!'
+
+    }
+
+}
+
+console.log(`Aqui como usamos o valor 4 como parâmetro, a função nos retornará como resposta ${parimpar(4)}`)
+
---
+++ aula16/funcao02.js
@@ -0,0 +1,8 @@
+function soma(numero1 = 0, numero2 = 0)
+{
+
+    return numero1 + numero2
+
+}
+
+console.log(`2 + 5 = ${soma(2)}`)
