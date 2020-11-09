82623aa85245f9461317e3fc15df89b464e402b6
Rodrigo Costa <rfcosta85@gmail.com>
2020-11-09 12:56:46 -0300

    Segunda parte do projeto

--- meusTestes/leticiaJoke/xdgamez/script.js
+++ meusTestes/leticiaJoke/xdgamez/script.js
@@ -1,9 +1,31 @@
 let products = []
+let list = document.getElementById('result')
 
 function register()
 {
-
+    
     window.alert('Produto cadastrado com sucesso!')
-    products.push()
+    products.push()    
+
+}
+
+function presentation()
+{
+
+    window.alert('Testando')
+
+}
+
+function ordered()
+{
+
+    window.alert('Ordenando...')
+
+}
+
+function exclude()
+{
+
+    window.alert('Produto Excluído com sucesso!')
 
 }
